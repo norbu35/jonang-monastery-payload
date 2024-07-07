3859e04c816ade9a6f6552abb83bf1bdb2c84a45
norbu <93921032+norbu35@users.noreply.github.com>
2024-07-07 20:29:27 +0800

    update

---
+++ src/templates/Newsletter.tsx
@@ -0,0 +1,22 @@
+import { render } from "@react-email/render";
+import { Html, Img, Text } from "@react-email/components";
+import { Document } from "payload/types";
+
+type Props = {
+  contents: Document;
+};
+
+const Template: React.FC<Props> = ({ contents }) => {
+  return (
+    <Html>
+      <Text>
+        This is a test newsletter
+      </Text>
+      {contents.bodyHtml}
+    </Html>
+  );
+};
+
+export function newsletter(data: Props) {
+  return render(<Template {...data} />);
+}
